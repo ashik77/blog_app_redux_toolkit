a4f948792a7a1f080b96eff98015345e660ac37d
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-10-30 20:40:01 +0200

    added button component and manually added two users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import UserList from "./features/users/UserList";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+      <h1 className="text-center font-bold text-2xl text-gray-700">
+        BLOG app (CRUD) with redux toolkit
+      </h1>
+      <UserList />
+    </div>
+  );
 }
 
 export default App;
